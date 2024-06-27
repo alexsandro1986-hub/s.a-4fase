630ec93872e55c6d265c84341794a5ae99fdd296
unknown <alexsandrosilvasilva886@gmail.com>
2024-06-27 17:41:51 -0300

    projeto base

---
+++ etiqueta/src/main/java/br/com/api/etiqueta/EtiquetaApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.etiqueta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtiquetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtiquetaApplication.class, args);
+	}
+
+}
---
+++ etiqueta/src/test/java/br/com/api/etiqueta/EtiquetaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.etiqueta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtiquetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
